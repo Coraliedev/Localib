50da8b1cc5dcaa00547331883fab944e6856dda3
Coraliedev <coralie.acquaert@gmail.com>
2022-09-27 13:08:05 +0200

    feat(Backend): create Client model

---
+++ backend/src/models/client.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const clientSchema = mongoose.Schema({
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+    minLenght: 3,
+    maxLenght: 50,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    minLenght: 3,
+    maxLenght: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+    minLenght: 10,
+    maxLenght: 10,
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+});
+
+export const ClienModel = mongoose.model("Client", clientSchema);
