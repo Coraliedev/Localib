d9df1ddb1478db586ffede766c0feff761dd4f00
Coraliedev <coralie.acquaert@gmail.com>
2022-09-30 14:42:34 +0200

    Frontend: modify vehicle model after modify backend

--- frontend/src/models/vehicle.model.tsx
+++ frontend/src/models/vehicle.model.tsx
@@ -1,34 +1,36 @@
 import { Etat } from "../enums/etat.enum";
 import { Type } from "../enums/type.enum";
 
+type Dates = [string, string];
+
 export default class VehicleModel {
   // Properties
   _id: string;
   brand: string;
   model: string;
   matriculation: string;
-  available: boolean;
   locationPrice: number;
   type: Type;
   state: Etat;
+  unavailableDates: Dates[];
   // Constructor
   constructor(
     _id: string = "",
     brand: string = "",
     model: string = "",
     matriculation: string = "",
-    available: boolean = true,
     locationPrice: number = 0,
     type: Type = Type.Voiture,
-    state: Etat = Etat.A
+    state: Etat = Etat.A,
+    unavailableDates: Dates[] = []
   ) {
     this._id = _id;
     this.brand = brand;
     this.model = model;
     this.matriculation = matriculation;
-    this.available = available;
     this.locationPrice = locationPrice;
     this.type = type;
     this.state = state;
+    this.unavailableDates = unavailableDates;
   }
 }
