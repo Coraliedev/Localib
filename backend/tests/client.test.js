79878122c1e6aa798611b3176f1aa6fbdbf94c27
Coraliedev <coralie.acquaert@gmail.com>
2022-10-26 09:52:13 +0200

    test: test for update client

--- backend/tests/client.test.js
+++ backend/tests/client.test.js
@@ -24,28 +24,40 @@ describe("GET /client", () => {
 });
 
 /* Testing the POST route. */
-describe("POST /client", () => {
-  it("should create a new client", async () => {
-    const res = await request(app).post("/client").send({
-      firstname: "Test Client",
-      lastname: "Test Client",
-      email: "test2@gmail.com",
-      phone: "514-555-5555",
-      birthdate: "1990-01-01",
-    });
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("firstname");
-    expect(res.body.firstname).toBe("Test Client");
-  });
-});
+
+// describe("POST /client", () => {
+//   it("should create a new client", async () => {
+//     const res = await request(app).post("/client").send({
+//       firstname: "Test Client",
+//       lastname: "Test Client",
+//       email: "test2@gmail.com",
+//       phone: "514-555-5555",
+//       birthdate: "1990-01-01",
+//     });
+//     expect(res.statusCode).toBe(200);
+//     expect(res.body).toHaveProperty("firstname");
+//     expect(res.body.firstname).toBe("Test Client");
+//   });
+// });
 
 /* Testint get client by id. */
 describe("GET /client/:id", () => {
   it("should return a client", async () => {
-    const res = await request(app).get(
-      "/client/6358e503265202d14f99f129"
-    );
+    const res = await request(app).get("/client/6358e503265202d14f99f129");
     expect(res.statusCode).toBe(200);
-    expect(res.body.firstname).toBe("Test Client");
+    expect(res.body.firstname).toBe("Test Client1");
+  });
+});
+
+/* Testing the PUT route. */
+describe("PUT /client/:id", () => {
+  it("should update a client", async () => {
+    const res = await request(app)
+      .put("/client/6358e595fe0e18783151b923")
+      .send({
+        firstname: "Test Client2",
+      });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.firstname).toBe("Test Client2");
   });
 });
